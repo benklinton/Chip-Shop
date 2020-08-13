9d5468d1480287bbf872af14300b8898f913683a
benklinton <benklinton@gmail.com>
2020-08-13 14:57:47 -0600

    started fix on google maps API

--- app.js
+++ app.js
@@ -1,43 +1,63 @@
+require("dotenv").config();
+let key = process.env.KEY;
 let map;
+let mapsURL = "https://maps.googleapis.com/maps/api/js?key=" + key + "-g&callback=initMap";
+
+$.ajax({
+  url: mapsURL,
+  method: "GET"
+})
+.then(function(response) {
+  console.log(response)
+})
+
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: -28.024, lng: 140.887},
+    center: { lat: -28.024, lng: 140.887 },
     zoom: 3
   });
-  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-  locations.map(function(location, i) {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length]
-    });
-  });
-
-
-  var locations = [
-    {lat: -31.563910, lng: 147.154312},
-    {lat: -33.718234, lng: 150.363181},
-    {lat: -33.727111, lng: 150.371124},
-    {lat: -33.848588, lng: 151.209834},
-    {lat: -33.851702, lng: 151.216968},
-    {lat: -34.671264, lng: 150.863657},
-    {lat: -35.304724, lng: 148.662905},
-    {lat: -36.817685, lng: 175.699196},
-    {lat: -36.828611, lng: 175.790222},
-    {lat: -37.750000, lng: 145.116667},
-    {lat: -37.759859, lng: 145.128708},
-    {lat: -37.765015, lng: 145.133858},
-    {lat: -37.770104, lng: 145.143299},
-    {lat: -37.773700, lng: 145.145187},
-    {lat: -37.774785, lng: 145.137978},
-    {lat: -37.819616, lng: 144.968119},
-    {lat: -38.330766, lng: 144.695692},
-    {lat: -39.927193, lng: 175.053218},
-    {lat: -41.330162, lng: 174.865694},
-    {lat: -42.734358, lng: 147.439506},
-    {lat: -42.734358, lng: 147.501315},
-    {lat: -42.735258, lng: 147.438000},
-    {lat: -43.999792, lng: 170.463352}
-  ]
 }
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+for (var i = 0; i < locations.length; i++) {
+  console.log(i)
+  new google.maps.Marker({
+    postion: location[i],
+    label: labels[i % labels.length]
+  })
+}
+
+// locations.map(function(location, i) {
+//   return new google.maps.Marker({
+//     position: location,
+//     label: labels[i % labels.length]
+//   });
+// });
+
+
+var locations = [
+  { lat: -31.563910, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+  { lat: -34.671264, lng: 150.863657 },
+  { lat: -35.304724, lng: 148.662905 },
+  { lat: -36.817685, lng: 175.699196 },
+  { lat: -36.828611, lng: 175.790222 },
+  { lat: -37.750000, lng: 145.116667 },
+  { lat: -37.759859, lng: 145.128708 },
+  { lat: -37.765015, lng: 145.133858 },
+  { lat: -37.770104, lng: 145.143299 },
+  { lat: -37.773700, lng: 145.145187 },
+  { lat: -37.774785, lng: 145.137978 },
+  { lat: -37.819616, lng: 144.968119 },
+  { lat: -38.330766, lng: 144.695692 },
+  { lat: -39.927193, lng: 175.053218 },
+  { lat: -41.330162, lng: 174.865694 },
+  { lat: -42.734358, lng: 147.439506 },
+  { lat: -42.734358, lng: 147.501315 },
+  { lat: -42.735258, lng: 147.438000 },
+  { lat: -43.999792, lng: 170.463352 }
+]
