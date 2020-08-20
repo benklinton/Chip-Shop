b7eb48345ad23fc866d089dae125a747bebf3c43
benklinton <benklinton@gmail.com>
2020-08-20 16:24:02 -0600

    added locactions

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function initMap() {
-  var locations = [{lat: 40.2592961, lng: -111.6692337}, {lat:40.2711152, lng:-111.7152443}]
-  var labels = "AB"
+  var locations = [
+    {lat: 40.5431388, lng: -111.8992322},
+    {lat: 40.5539592, lng: -111.9002981},
+    {lat: 40.7402955, lng: -111.9087438},
+    {lat: 40.8652313, lng: -111.8961617},
+    {lat: 40.865295,  lng: -111.9289926},
+    {lat: 40.8970171, lng: -111.8953847},
+    {lat: 40.8648216, lng: -111.8992162}
+  ]
+  var labels = "ABCDEFG"
   var map = new google.maps.Map(
       document.getElementById('map'), {
         center: { lat: 40.3365078, lng: -111.8290607 },
