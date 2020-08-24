4ee8a51f67fa5e5676a03183f41f1221c2b1f186
benklinton <benklinton@gmail.com>
2020-08-24 16:40:12 -0600

    added all locations to locations section

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 function initMap() {
   var locations = [
-    {lat: 40.8969909, lng: -111.8932824}, //Tim Dahle Nissan A
-    {lat: 40.870601, lng: -111.8900219}, //Tim Dahle Infiniti B
-    {lat: 40.86481759999999, lng: -111.8970275}, //Tim Dahle Southtowne Nissan C
-    {lat: 40.7396652, lng: -111.9067898}, //Tim Dahle Mazada D
-    {lat: 40.6731619, lng: -111.8891222}, //Murdock Hyundai of Murray E
-    {lat: 40.672421, lng: -111.8876254}, //LHM Dodge Jeep Chrysler Bountiful F
-    {lat: 40.6722237, lng: -111.8892444}, //Murdock Chevy Bountiful G
-    {lat: 40.6705572, lng: -111.8893578}, //Tim Dahle Ford H
-    {lat: 40.5543772, lng: -111.8983593},  //Karl Malone Toyota I
-    {lat: 40.5494404, lng: -111.8974743}, //Murdock Hyundai of Lindon J
-    {lat: 40.543413, lng: -111.8965277}, //Volkswagen Southtowne K
+    {lat: 40.8969909, lng: -111.8932824}, //LHM Dodge Jeeep Chrysler A
+    {lat: 40.870601, lng: -111.8900219}, //Performace Ford B
+    {lat: 40.86481759999999, lng: -111.8970275}, //Murdock Chevy C
+    {lat: 40.7396652, lng: -111.9067898}, // LHM Super Ford D
+    {lat: 40.6731619, lng: -111.8891222}, //Tim Dahle Nissan E
+    {lat: 40.672421, lng: -111.8876254}, //D Dahle Mazada F
+    {lat: 40.6722237, lng: -111.8892444}, //Tim Dahle Infiniti G
+    {lat: 40.6705572, lng: -111.8893578}, //Murdock Hyundai H
+    {lat: 40.5543772, lng: -111.8983593},  //Voltzwagan SouthTowne I
+    {lat: 40.5494404, lng: -111.8974743}, //Tim Dahle Nissan SouthTowne J
+    {lat: 40.543413, lng: -111.8965277}, // Karl Malone Toyota K
     {lat: 40.3494023, lng: -111.7273238}, //Low Book Sales of Lindon L
     {lat: 40.3456666, lng: -111.7258247}, //Dastrup Auto M
-    {lat: 40.3293784, lng: -111.7337983}, //Performance Ford N
+    {lat: 40.3293784, lng: -111.7337983}, // Murdock Hyundai N
     {lat: 40.2711111, lng: -111.7130556}, //Brent Brown Toyota O
     {lat: 40.2593325, lng: -111.6670972}, //LHM Ford Provo P
     {lat: 40.259429, lng: -111.665048}, //LHM Dodge Jeep Chrysler Provo Q
-    {lat: 40.1423249, lng: -111.6484656}, //LHM Super Ford R
+    {lat: 40.1423249, lng: -111.6484656}, //Tim Dahle Ford R
 
   ]
   var labels = "ABCDEFGHIJKLMNOPQR"
